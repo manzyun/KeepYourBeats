4fc12c4808bbea4dac9eeeb79c5aae18d01f2d1d
manzyun <manzyun@gmail.com>
2022-01-30 00:18:57 +0900

    [WIP][Add] Draft or prototype scenes structure.

---
+++ Scene/DirtyMetro.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+
+# TODO: メトロノームの『カチッ』と『チーン』の音をあらかじめ入れる。
+
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	# TODO: 4つの乱整数を生成
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func ding():
+	# TODO: 生成した4つの乱数 * 100の間隔でメトロノームの音を鳴らす。
+	pass # Replace with function body.
